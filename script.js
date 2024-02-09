97e3b2e1e596149e0bab285539fbb764f8f8b46d
Sarfaraz Ansari <65963305+sarfaraz26@users.noreply.github.com>
2024-02-09 21:32:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var hadithText = document.querySelector('#hadith-text');
+var hadithChapter = document.querySelector('#hadith-chapter');
+var hadithRef = document.querySelector('#hadith-ref');
+
+
+async function GetHadith()
+{
+    try
+    {
+        const response = await fetch('https://random-hadith-generator.vercel.app/bukhari/')
+        const {data} = await response.json();
+
+        console.log(data);
+
+        hadithText.innerHTML = data.hadith_english;
+        hadithChapter.innerHTML = data.chapterName;
+        hadithRef.innerHTML = "References : "+data.refno
+
+    }
+    catch
+    {
+        console.error('Error 500 : ')
+    }
+
+
+}
+
+GetHadith();
